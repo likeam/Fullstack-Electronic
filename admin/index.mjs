26473ee3442a91ba53cacd401285cc5afc8dc7dc
Abdul Rehamn <likeam99@gmail.com>
2024-06-30 05:08:59 +0500

    Building DotEnv of System

--- admin/index.mjs
+++ admin/index.mjs
@@ -2,7 +2,8 @@ import express from 'express';
 
 const app = express();
 
-const port = 8000;
+import 'dotenv/config'
+const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
