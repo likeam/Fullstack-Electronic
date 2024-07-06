cbff28839847f45ecefe86d15860a3988309e075
Abdul Rehamn <likeam99@gmail.com>
2024-07-06 15:31:48 +0500

    Removing Error

--- client/src/ui/BannerCategories.tsx
+++ client/src/ui/BannerCategories.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import {config} from "../../config";
 import { getData } from "../lib";
 import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
 import { CategoryProps } from "../../type";
 import { Link } from "react-router-dom";
 import CustomLeftArrow from "./CustomLeftArrow"
