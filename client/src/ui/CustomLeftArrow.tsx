29523022a07c22d6b82e8ce75c12cd8f4266a3d9
Abdul Rehamn <likeam99@gmail.com>
2024-07-09 13:21:48 +0500

    Error

--- client/src/ui/CustomLeftArrow.tsx
+++ client/src/ui/CustomLeftArrow.tsx
@@ -3,7 +3,7 @@ import { HiArrowLeft } from "react-icons/hi";
 const CustomLeftArrow = ({ onClick }: any) => {
   return (
     <button
-     
+     onClick={onClick}
       className="absolute right-0 top-5 m-auto h-10 w-10 flex items-center justify-center bg-gray-100 rounded-full border-[1px] border-gray-200 hover:bg-gray-950 hover:text-white duration-200"
       aria-label="Next"
     >
