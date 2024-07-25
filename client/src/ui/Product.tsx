af86ff3bef3439ba4252dc5ef01c231c0a60f74a
Abdul Rehamn <likeam99@gmail.com>
2024-07-25 19:18:47 +0500

    Removing Error

--- client/src/ui/Product.tsx
+++ client/src/ui/Product.tsx
@@ -1,7 +1,11 @@
 import { useParams } from "react-router-dom";
+import { config } from "../config";
 
 const Product = () => {
   const { id } = useParams();
+  const endpoint = id
+    ? `${config?.baseUrl}/products/${id}`
+    : `${config?.baseUrl}/products/`;
   console.log(id);
   return <div>Product</div>;
 };
