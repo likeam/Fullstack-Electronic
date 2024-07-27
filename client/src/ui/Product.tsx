eb73f04954c776e36fee2a29bc805bd5d967517c
Abdul Rehamn <likeam99@gmail.com>
2024-07-27 20:01:43 +0500

    Removing Errors

--- client/src/ui/Product.tsx
+++ client/src/ui/Product.tsx
@@ -1,15 +1,33 @@
 import { useParams } from "react-router-dom";
 import { config } from "../config";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ProductProps } from "../type";
 
 const Product = () => {
   const [productData, setProductData] = useState<ProductProps | null>(null);
+  const [allProducts, setAllProducts] = useState<ProductProps[]>([]);
+  const [loading, setLoading] = useState(false);
+
   const { id } = useParams();
 
   const endpoint = id
     ? `${config?.baseUrl}/products/${id}`
     : `${config?.baseUrl}/products/`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const data = await fetch(endpoint).then((response) => response.json());
+      } catch (error) {
+        console.error("Fetching data failed", error);
+      } finally {
+      }
+    };
+
+    fetchData();
+  }, [id, endpoint]);
+
   console.log(id);
   return <div>Product</div>;
 };
