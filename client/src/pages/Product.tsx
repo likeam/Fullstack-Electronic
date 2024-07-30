4feea2461436d1953d0a12e253319088667e8257
Abdul Rehamn <likeam99@gmail.com>
2024-07-30 12:23:44 +0500

    Building Styling of Product Section

--- client/src/pages/Product.tsx
+++ client/src/pages/Product.tsx
@@ -3,11 +3,18 @@ import { config } from "../config";
 import { useEffect, useState } from "react";
 import { ProductProps } from "../type";
 import { getData } from "../lib";
+import Loading from "../ui/Loading";
+import Container from "../ui/Container";
+import _, { divide } from "lodash";
+import PriceTag from "../ui/PriceTag";
+import { MdOutlineStarOutline } from "react-icons/md";
+import { FaRegEye } from "react-icons/fa";
+import FormattedPrice from "../ui/FormattedPrice";
 
 const Product = () => {
   const [productData, setProductData] = useState<ProductProps | null>(null);
   const [allProducts, setAllProducts] = useState<ProductProps[]>([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [imgUrl, setImgUrl] = useState("");
   const [color, setColor] = useState("");
 
@@ -44,7 +51,105 @@ const Product = () => {
     }
   }, [productData]);
 
-  return <div>Product</div>;
+  return (
+    <div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Container>
+          {!!id && productData && _.isEmpty(allProducts) ? (
+            <div className=" grid grid-cols-1 md:grid-cols-2 gap-10">
+              <div className=" flex flex-start">
+                <div>
+                  {productData?.images?.map((item, index) => (
+                    <img
+                      src={item}
+                      alt="img"
+                      key={index}
+                      className={`w-24 cursor-pointer opacity-80 hover:opacity-100 duration-300 ${
+                        imgUrl === item &&
+                        "border border-gray-500 rounded-sm opacity-100"
+                      }`}
+                      onClick={() => setImgUrl(item)}
+                    />
+                  ))}
+                </div>
+                <div>
+                  <img src={imgUrl} alt="mainImage" />
+                </div>
+              </div>
+              <div className=" flex flex-col gap-4">
+                <h2 className=" text-3xl font-bold">{productData?.name}</h2>
+                <div className=" flex items-center justify-between">
+                  <PriceTag
+                    regularPrice={productData?.regularPrice}
+                    discountedPrice={productData?.discountedPrice}
+                    className="text-xl"
+                  />
+                  <div className="flex items-center gap-1">
+                    <div className="text-base text-lightText flex items-center">
+                      <MdOutlineStarOutline />
+                      <MdOutlineStarOutline />
+                      <MdOutlineStarOutline />
+                      <MdOutlineStarOutline />
+                      <MdOutlineStarOutline />
+                    </div>
+                    <p className="text-base font-semibold">{`(${productData?.reviews} reviews)`}</p>
+                  </div>
+                </div>
+                <p className=" flex items-center">
+                  <FaRegEye className=" mr-1" />
+                  <span className=" font-semibold mr-1">
+                    {productData?.reviews}
+                  </span>
+                  peoples are viewing this right now
+                </p>
+                <p>
+                  You are saving{" "}
+                  <span className="text-base font-semibold text-green-500">
+                    <FormattedPrice
+                      amount={
+                        productData?.regularPrice! -
+                        productData?.discountedPrice!
+                      }
+                    />
+                  </span>{" "}
+                  upon purchase
+                </p>
+                <div>
+                  {color && (
+                    <p>
+                      Color:{""}
+                      <span
+                        className=" font-semibold capitalize"
+                        style={{ color: color }}
+                      >
+                        {color}
+                      </span>
+                    </p>
+                  )}
+                  <div className="">
+                    {productData?.colors.map((item) => (
+                      <div
+                        key={item}
+                        className={`${
+                          item === color
+                            ? "border border-black p-1 rounded-full"
+                            : "border-transparent"
+                        }`}
+                      ></div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <p>AppProducts</p>
+          )}
+        </Container>
+      )}
+    </div>
+  );
 };
 
 export default Product;
