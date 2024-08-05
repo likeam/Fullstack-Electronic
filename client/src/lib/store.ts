5ff477a30a22d191d4d64415cd84bf8f668b7683
Abdul Rehamn <likeam99@gmail.com>
2024-08-05 14:12:41 +0500

    Removing Error

--- client/src/lib/store.ts
+++ client/src/lib/store.ts
@@ -1,7 +1,35 @@
+import { doc } from "firebase/firestore/lite";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+import { db } from "./firebase";
 
+const customStorage = {
+    getItem: (name: string) => {
+        const item = localStorage.getItem(name);
+        return item? JSON.parse(item) : null;
+    },
+    setItem: (name: string, value: any) => {
+        localStorage.setItem(name, JSON.stringify(value));
+    },
+    removeItem: (name: string) => {
+        localStorage.removeItem(name);
+    },
+}
 
 export const store = create()(persist((set) => ({
-    
-})))
+    currentUser:null,
+    isLoading: true,
+    cartProduct: [],
+    favoriteProduct: [],
+
+    getUserInfo: async(uid: any) => {
+        if(!uid) return set({ currentUser:null, isLoading:false})
+
+        const docRef = doc(db, "users", uid);
+    }
+
+}),{
+    name: 'khalid-storage',
+    storage: customStorage
+}
+))
