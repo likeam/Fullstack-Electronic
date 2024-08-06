92341cd315abd2fd13c08a5c5983b64f2be42737
Abdul Rehamn <likeam99@gmail.com>
2024-08-06 22:03:12 +0500

    Error Removing

--- client/src/lib/store.ts
+++ client/src/lib/store.ts
@@ -1,7 +1,42 @@
-import { doc } from "firebase/firestore/lite";
+import { doc, getDoc } from "firebase/firestore/lite";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { db } from "./firebase";
+import { ProductProps } from "../type";
+
+
+
+interface CartProduct extends ProductProps {
+    quantity: number;
+  }
+  
+  interface UserType {
+    firstName: string;
+    lastName: string;
+    password: string;
+    email: string;
+    avatar: string;
+    id: string;
+  }
+  
+  interface StoreType {
+    // user
+    currentUser: UserType | null;
+    isLoading: boolean;
+    getUserInfo: (uid: any) => Promise<void>;
+    // cart
+    cartProduct: CartProduct[];
+    addToCart: (product: ProductProps) => Promise<void>;
+    decreaseQuantity: (productId: number) => void;
+    removeFromCart: (productId: number) => void;
+    resetCart: () => void;
+    // // favorite
+    favoriteProduct: CartProduct[];
+    addToFavorite: (product: ProductProps) => Promise<void>;
+    removeFromFavorite: (productId: number) => void;
+    resetFavorite: () => void;
+  }
+
 
 const customStorage = {
     getItem: (name: string) => {
@@ -26,6 +61,16 @@ export const store = create()(persist((set) => ({
         if(!uid) return set({ currentUser:null, isLoading:false})
 
         const docRef = doc(db, "users", uid);
+        const docSnap = await getDoc(docRef);
+
+        try {
+            if(docSnap.exists()){
+                set({currentUser: docSnap.date() as UserType, isLoading: false});
+            }
+        } catch (error) {
+            console.log('Get user Info', error);
+            set({currentUser: null, isLoading: false})
+        }
     }
 
 }),{
